228f7181551124c66ac553985e1ab8ac1aec6b7d
Richard Höchenberger <richard.hoechenberger@gmail.com>
2022-03-08 10:38:46 +0100

    Fix title of Sysinfo report

--- recipe/menu/mne_sys_info.py
+++ recipe/menu/mne_sys_info.py
@@ -5,16 +5,20 @@ import mne
 
 
 def main():
-    report = mne.Report('MNE System Information')
-    report.add_sys_info('System Information')
+    report = mne.Report(title='MNE System Information')
+    report.add_sys_info(title='System Information')
 
     report_file = tempfile.NamedTemporaryFile(
         prefix='mne_sys_info',
         suffix='.html',
         delete=False
     )
-    report_file.close()
-    report.save(fname=report_file.name, open_browser=True, overwrite=True)
+    report_file.close()  # close it so we can open it again for writing
+    report.save(
+        fname=report_file.name,
+        open_browser=True,
+        overwrite=True
+    )
 
 
 if __name__ == '__main__':
