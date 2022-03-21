43fd060450b221b1e86dedc75c7598c172e2833d
Eric Larson <larson.eric.d@gmail.com>
2022-03-21 13:55:11 -0400

    FIX: where

--- recipe/menu/open_prompt.bat
+++ recipe/menu/open_prompt.bat
@@ -3,7 +3,7 @@
 
 call %__PREFIX__%\Scripts\Activate.bat
 FOR /F "tokens=*" %%g IN ('python --version') do (SET PYVER=%%g)
-FOR /F "tokens=*" %%g IN ('python --version') do (SET PYPATH=%%g)
+FOR /F "tokens=*" %%g IN ('where python') do (SET PYPATH=%%g)
 FOR /F "tokens=*" %%g IN ('mne --version') do (SET MNEVER=%%g)
 
 ECHO Using %PYVER% from %PYPATH%
