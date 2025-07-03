403ca4bf0ea6892cf08cfbffb3b7bd8d251d1b36
Eric Larson <larson.eric.d@gmail.com>
2025-07-03 15:16:07 -0400

    MAINT: Fix prompt launcher (#142)
    * MAINT: Fix prompt launcher
    
    * MNT: Re-rendered with conda-build 25.5.0, conda-smithy 3.50.1, and conda-forge-pinning 2025.07.03.14.06.40
    
    * FIX: Linty
    
    * MNT: Re-rendered with conda-build 25.5.0, conda-smithy 3.50.1, and conda-forge-pinning 2025.07.03.14.06.40
    
    * FIX: Faster
    
    * MNT: Re-rendered with conda-build 25.5.0, conda-smithy 3.50.1, and conda-forge-pinning 2025.07.03.14.06.40
    
    * FIX: What
    
    * FIX: Naming
    
    * FIX: What
    
    * MNT: Re-rendered with conda-build 25.5.0, conda-smithy 3.50.1, and conda-forge-pinning 2025.07.03.14.06.40
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/menu/open_prompt.bat
+++ recipe/menu/open_prompt.bat
@@ -1,7 +1,16 @@
 :: This is used to initialize the bash prompt on Windows.
 @ECHO OFF
 
-call %__PREFIX__%\Scripts\Activate.bat
+:: Workaround for
+:: https://github.com/conda/conda/issues/14884
+set "CONDA_EXE=#PREFIX#\Scripts\conda.exe"
+call "#PREFIX#\Scripts\Activate.bat"
+:: Find first Python on path.
+FOR /F "tokens=*" %%g IN ('where python') do (
+    SET PYPATH=%%g
+    goto :endloop
+)
+:endloop
 FOR /F "tokens=*" %%g IN ('python --version') do (SET PYVER=%%g)
 FOR /F "tokens=*" %%g IN ('where python') do (SET PYPATH=%%g)
 FOR /F "tokens=*" %%g IN ('mne --version') do (SET MNEVER=%%g)
